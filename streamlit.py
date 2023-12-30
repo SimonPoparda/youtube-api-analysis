71ac2a1e4d2432b3cab38a9eab24a7c5988b4609
MapleSight <108056198+SimonPoparda@users.noreply.github.com>
2023-12-30 18:14:43 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from googleapiclient.discovery import build
+import seaborn as sns
 
 st.set_page_config(
     page_title='Multiple App',
@@ -11,17 +13,56 @@ st.sidebar.success('Data Analysis of YT Channels')
 
 st.markdown('<p style="font-size:20px;">📊 Basic Statistics for each channel</p>', unsafe_allow_html=True)
 
-channels_data_df = pd.read_csv('channels_data_df.csv')
+api_key = 'AIzaSyCzltnXE_nObh4xlw0331YmKDi8vqjYtyk'
+channel_ids = ['UCnUrMqV57fp3uPddvmDpTaA','UC7RswyY8VfbSdikz_8wdp3w', 
+'UCZ7KWO9E51KNtkDN_TfA69Q', 'UC-Q7kWPVNqMsCyy4ZgGE6MA']
 
+api_service_name = "youtube"
+api_version = "v3"
+
+# Get credentials and create an API client
+youtube = build(
+    api_service_name, api_version, developerKey=api_key)
+
+def get_channel_stats(youtube, channel_ids):
+    all_data = []
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        id=','.join(channel_ids)
+    )
+    response = request.execute()
+
+    for i in range(len(response['items'])):
+        data = dict(Channel_name = response['items'][i]['snippet']['title'],
+                    Subscribers = response['items'][i]['statistics']['subscriberCount'],
+                    Views = response['items'][i]['statistics']['viewCount'],
+                    Total_videos = response['items'][i]['statistics']['videoCount'])
+        all_data.append(data)
+        
+    return all_data
+
+channel_statistics = get_channel_stats(youtube, channel_ids)
+channel_statistics = get_channel_stats(youtube, channel_ids)
+channels_data_df = pd.DataFrame(channel_statistics)
+st.write(channels_data_df)
+
+channels_data_df['Subscribers'] = pd.to_numeric(channels_data_df['Subscribers'])
+channels_data_df['Views'] = pd.to_numeric(channels_data_df['Views'])
+channels_data_df['Total_videos'] = pd.to_numeric(channels_data_df['Total_videos'])
+
+ # Selectbox for choosing the visualization
 st.markdown('<p style="font-size:20px;">📈 Visualization</p>', unsafe_allow_html=True)
 selected_chart = st.selectbox("Select Visualization", ["Total Subscribers", "Total Views", "Total Videos"])
 
+sns.set(rc={'figure.figsize': (10, 8)})
 if selected_chart == "Total Subscribers":
-    st.bar_chart(channels_data_df.set_index('Channel_name')['Subscribers'])
-    st.title("Total Subscribers")
+    ax = sns.barplot(x='Channel_name', y='Subscribers', data=channels_data_df)
+    ax.set_title("Total Subscribers")  # Set the title for the bar plot
 elif selected_chart == "Total Views":
-    st.bar_chart(channels_data_df.set_index('Channel_name')['Views'])
-    st.title("Total Views")
+    ax = sns.barplot(x='Channel_name', y='Views', data=channels_data_df)
+    ax.set_title("Total Views")  # Set the title for the bar plot
 else:
-    st.bar_chart(channels_data_df.set_index('Channel_name')['Total_videos'])
-    st.title("Total Videos")
+    ax = sns.barplot(x='Channel_name', y='Total_videos', data=channels_data_df)
+    ax.set_title("Total Videos")  # Set the title for the bar plot
+
+st.pyplot(fig=ax.get_figure())
